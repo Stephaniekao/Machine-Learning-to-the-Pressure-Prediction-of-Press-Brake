6f3fcaf1a741a23a602cf0fc61e0fa8640c43f9c
Stephaniekao <96237110+Stephaniekao@users.noreply.github.com>
2025-12-02 23:50:34 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+Press Brake ML Package
+
+This package contains:
+- Predata_optimized: data preprocessing
+- SVR_model: Support Vector Regression model
+- GBDT_model: Gradient Boosting Decision Tree model
+- XGBoost_model: XGBoost model
+- RF_model: Random Forest model
+- ETR_model: Extra Trees model
+"""
